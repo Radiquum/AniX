9e9142a579784b6d2390da1f09c07cce1f551422
Kentai Radiquum <kentai.waah@gmail.com>
2024-04-18 23:02:12 +0500

    backend: fix docs spelling mistakes

--- backend/modules/release/release.py
+++ backend/modules/release/release.py
@@ -6,13 +6,13 @@ from modules.proxy import ENDPOINTS
 router = APIRouter()
 
 
-@router.get("/{release_id}", summary="Get release info by release ID")
+@router.get("/{release_id}", summary="Get release info")
 async def GetReleaseById(request: Request, release_id: str):
     return await apiRequest(request, ENDPOINTS["release"]["info"], release_id)
 
 
 @router.get(
-    "/{release_id}/voiceover", summary="Get release voiceover info by release ID"
+    "/{release_id}/voiceover", summary="Get release voiceover info"
 )
 async def GetReleaseVoiceover(request: Request, release_id: str):
     return await apiRequest(request, ENDPOINTS["release"]["episode"], release_id)
@@ -20,7 +20,7 @@ async def GetReleaseVoiceover(request: Request, release_id: str):
 
 @router.get(
     "/{release_id}/{voiceover_id}",
-    summary="Get available players for selected voiceover of an release",
+    summary="Get available players for selected voiceover of a release",
 )
 async def GetReleaseVoiceoverPlayer(
     request: Request, release_id: str, voiceover_id: str
@@ -32,7 +32,7 @@ async def GetReleaseVoiceoverPlayer(
 
 @router.get(
     "/{release_id}/{voiceover_id}/{source_id}",
-    summary="Get available episodes for selected voiceover and a player of an release",
+    summary="Get available episodes for selected voiceover and a player of a release",
 )
 async def GetReleaseEpisodes(
     request: Request, release_id: str, voiceover_id: str, source_id: str
@@ -61,7 +61,7 @@ async def MarkEpisodeAsWatched(
 
 @router.get(
     "/{release_id}/{episode}/addHistory",
-    summary="mark episode of a selected voiceover as watched",
+    summary="add episode of a selected voiceover to history",
 )
 async def AddEpisodeToHistory(
     request: Request, release_id: str, source_id: str, episode: str, token: str
