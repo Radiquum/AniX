cd784295408baa010fa10a8d538072c65f8d4720
Kentai Radiquum <kentai.waah@gmail.com>
2024-04-19 02:30:14 +0500

    backend: set api prefix to `/api/`

--- backend/main.py
+++ backend/main.py
@@ -33,16 +33,18 @@ TAGS = [
 app = FastAPI(
             openapi_tags=TAGS, 
             title="AniX API",
-            description="unofficial API proxy for Anixart android application.",)
+            description="unofficial API proxy for Anixart android application.",
+            openapi_url="/api/openapi.json",
+            docs_url="/api/docs", redoc_url=None)
 
-app.include_router(profile.router, prefix="/profile", tags=["Profile"])
-app.include_router(auth.router, prefix="/auth", tags=["Profile"])
+app.include_router(profile.router, prefix="/api/profile", tags=["Profile"])
+app.include_router(auth.router, prefix="/api/auth", tags=["Profile"])
 
-app.include_router(release.router, prefix="/release", tags=["Releases"])
+app.include_router(release.router, prefix="/api/release", tags=["Releases"])
 
-app.include_router(index.router, prefix="/index", tags=["Index"])
-app.include_router(favorites.router, prefix="/favorites", tags=["Favorites"])
-app.include_router(search.router, prefix="/search", tags=["Search"])
+app.include_router(index.router, prefix="/api/index", tags=["Index"])
+app.include_router(favorites.router, prefix="/api/favorites", tags=["Favorites"])
+app.include_router(search.router, prefix="/api/search", tags=["Search"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
