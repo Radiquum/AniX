c02c5d90b13c2afe6a7ed8b96f1d4aa49bb7f96c
Radiquum <kentai.waah@gmail.com>
2025-09-08 18:01:59 +0500

    feat/api-prox: add custom roles hook

--- api-prox/src/hooks/enabledHooks.ts
+++ api-prox/src/hooks/enabledHooks.ts
@@ -1,5 +1,6 @@
 import { Hook } from "./index.js";
 import thirdPartyReleaseRatingHook from "./show3rdPartyReleaseRating.js";
+import customUserRoles from "./addUserRoles.js"
 
-export const enabledHooks: Hook[] = [thirdPartyReleaseRatingHook];
+export const enabledHooks: Hook[] = [thirdPartyReleaseRatingHook, customUserRoles];
 export default enabledHooks;
